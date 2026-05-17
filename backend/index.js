7be0f25f28e67bd8719d430da169473e3b799843
khushibagora12 <khushibagora76@gmail.com>
2026-05-17 20:25:34 +0530

    url wo protocol

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,17 @@ app.use(express.json());
 app.use(cors());
 app.use(rateLimiter);
 
+app.get('/clear', async (req, res) => {
+    await client.flushDb();
+    res.send("cache cleared");
+})
 app.post('/', async (req, res) => {
-    const { url } = req.body;
+    let { url } = req.body;
     const hashedUrl = await adler32(url);
     const frontend_url = process.env.FRONTEND_URL;
-
+    if(!url.startsWith('http://') || !url.startsWith('https://')){
+        url = 'https://'+url;
+    }
     const urlExist = await UrlDb.findOne({ og_url: url })
     if (urlExist) {
         const index_no = await UrlDb.aggregate([
@@ -68,19 +74,21 @@ app.post('/', async (req, res) => {
 
 app.get('/:hash/:index', async (req, res) => {
     const { hash, index } = req.params;
-    console.log(hash, " ", index)
+    console.log("hash: " ,hash, " ", index)
     try {
-        const data = await client.get(hash)[index]
+        const data = await client.get(hash+index)
         if (data) {
-            console.log("cache hit")
+            console.log("cache hit: ", data)
             return res.redirect(data);
-        } else {
+        } 
+        else {
             const getUrl = await UrlDb.findOne({ uniqueKey: hash })
-            // console.log("found url: ", getUrl.og_url[index]);
+            console.log("found url: ", getUrl.og_url[index]);
 
             if (!getUrl) return res.json({ message: "not found" })
             const redUrl = getUrl.og_url[index];
 
+            console.log(hash+index, "- ", redUrl);
             await client.set(hash+index, redUrl)
 
             return res.redirect(redUrl)
